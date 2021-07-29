98185912af07130e42013560e3773f8239a727c0
valeria zambrano <Valeria.zambrano.guzman@gmail.com>
2021-07-29 18:38:18 -0400

    Comment unused JS package imports

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,17 +8,17 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
-import 'select2/dist/css/select2.min.css'
-import 'jquery-bar-rating/dist/themes/css-stars';
-import 'aos/dist/aos.css';
-import AOS from 'aos';
-import { initTyped } from './plugins/init_typedjs';
-import { initSelect2 } from './plugins/init_select2';
-import { initStarRating } from './plugins/init_star_rating';
+// import 'select2/dist/css/select2.min.css'
+// import 'jquery-bar-rating/dist/themes/css-stars';
+// import 'aos/dist/aos.css';
+// import AOS from 'aos';
+// import { initTyped } from './plugins/init_typedjs';
+// import { initSelect2 } from './plugins/init_select2';
+// import { initStarRating } from './plugins/init_star_rating';
 
 document.addEventListener('turbolinks:load', () => {
-  initSelect2();
-  initTyped();
-  AOS.init();
-  initStarRating();
+  // initSelect2();
+  // initTyped();
+  // AOS.init();
+  // initStarRating();
 });
